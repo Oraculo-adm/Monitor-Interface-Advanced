57b40fc9ffc63f32919b7444414d8f21d6f037da
Leone Martins <leonne.martins@outlook.com>
2025-05-12 13:48:07 -0300

    ETAPA 4: Estrutura inicial dos módulos (NetInterfaces, Ping, Routes, Graphic)

---
+++ modules/Routes/Main.cs
@@ -0,0 +1,11 @@
+
+namespace Routes
+{
+    public class Main
+    {
+        public static void Start()
+        {
+            // Futuro: realizar teste de rota com interface selecionada
+        }
+    }
+}
